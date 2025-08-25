2d0c5f3940278773144ef42079c83b6a4b1c77b6
Aditya Sawant <sawant.aditya7766@gmail.com>
2025-08-25 12:20:21 +0530

    initial commit

---
+++ article_view_1.sql
@@ -0,0 +1,31 @@
+-- Create the Views table
+CREATE TABLE Views (
+    article_id INT,
+    author_id INT,
+    viewer_id INT,
+    view_date DATE
+);
+
+-- Insert the data into Views table
+INSERT INTO Views (article_id, author_id, viewer_id, view_date)
+VALUES
+(1, 3, 5, '2019-08-01'),
+(1, 3, 6, '2019-08-02'),
+(2, 7, 7, '2019-08-01'),
+(2, 7, 6, '2019-08-02'),
+(4, 7, 1, '2019-07-22'),
+(3, 4, 4, '2019-07-21'),
+(3, 4, 4, '2019-07-21');
+
+-- Write a solution to find all the authors that viewed at least one of their own articles.
+-- Return the result table sorted by id in ascending order.
+
+SELECT DISTINCT
+    author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+GROUP BY author_id
+HAVING COUNT(author_id) >= 1
+ORDER BY id;
+
+DROP TABLE Views;
